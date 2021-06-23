8cf7ee08a119d71c6b1ce67423b596c03168c0af
Timothée Pionnier <thimothee.pionnier@orange.com>
2021-06-23 16:37:28 +0200

    fix wrong pzckage name

--- android/app/src/androidTest/java/com/example/integration_test_app/MainActivityTest.java
+++ android/app/src/androidTest/java/com/example/integration_test_app/MainActivityTest.java
@@ -1,4 +1,4 @@
-package com.example.integration_test;
+package com.example.integration_test_app;
 
 import androidx.test.rule.ActivityTestRule;
 import dev.flutter.plugins.integration_test.FlutterTestRunner;
