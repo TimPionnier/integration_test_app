f0528a268c9f7c6e5b631c8033928ef53bef5439
Timothée Pionnier <thimothee.pionnier@orange.com>
2021-09-14 15:05:21 +0200

    add firebase dependences

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -1,15 +1,31 @@
 import 'dart:io';
+import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:integration_test/integration_test_driver_extended.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 Future<void> main() async {
-  
   try {
+    firebase_storage.FirebaseStorage storage =
+        firebase_storage.FirebaseStorage.instance;
     await integrationDriver(
       onScreenshot: (String screenshotName, List<int> screenshotBytes) async {
         final File image = await File('screenshots/$screenshotName.png')
             .create(recursive: true);
         image.writeAsBytesSync(screenshotBytes);
+
+        Uint8List data = Uint8List.fromList(screenshotBytes);
+
+        firebase_storage.Reference ref =
+            storage.ref('screenshots/$screenshotName.png');
+
+        // Upload raw data.
+        await ref.putData(data);
+        // Get raw data.
+        Uint8List? downloadedData = await ref.getData();
+        // prints -> Hello World!
+        print(utf8.decode(downloadedData!));
         return true;
       },
     );
